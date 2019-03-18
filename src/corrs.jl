6db38d72758607ad5a9816cf2d8939c5285bbaaa
Galen Lynch <galen.lynch@gmail.com>
2019-03-18 10:05:25 -0400

    Revamp cross-correlation to return valid portion only

--- src/corrs.jl
+++ src/corrs.jl
@@ -1,124 +1,43 @@
-function circ_xcorr_normed(
+# Version for single vectors
+function xcorr_normed_valid(
     u::AbstractArray{<:AbstractFloat},
-    v::AbstractArray{<:AbstractFloat};
-    eltype::Type = Float32,
+    v::AbstractArray{<:AbstractFloat},
+    nlag::Integer;
+    outeltype::Type = Float32,
     center::Bool = true
 )
-    nu = length(u)
-    nv = length(v)
-    nlag = max(nu, nv) - 1
-    n = 2 * nlag + 1
-    n_padded = nextfastfft(n)
-    n_comp = div(n_padded, 2) + 1
-
-    r_buf = Vector{eltype}(undef, n_padded)
-    uim_buf = Vector{Complex{eltype}}(undef, n_comp)
-    vim_buf = similar(uim_buf)
-    accum = zeros(eltype, n_padded)
-    out = similar(accum, n)
-    p = plan_rfft(r_buf)
-    ip = plan_irfft(uim_buf, n_padded)
-
-    out_norm = _xcorr!(
-        accum, u, nu, v, nv, r_buf, uim_buf, vim_buf, p, ip, center
-    )
-    circ_xcorr_unwrap!(out, accum, nlag, n_padded)
-    out ./= out_norm
-    out
-end
-
-function _xcorr_normed!(
-    out::AbstractVector{<:Real},
-    accum::AbstractVector{<:Real},
-    r_buf::AbstractVector{<:Real},
-    uim_buf::AbstractVector{<:Complex},
-    vim_buf::AbstractVector{<:Complex},
-    us::AbstractArray{<:AbstractArray{<:Real}},
-    vs::AbstractArray{<:AbstractArray{<:Real}},
-    nu::Integer,
-    u_ls::AbstractVector{<:Integer},
-    v_ls::AbstractVector{<:Integer},
-    p,
-    ip,
-    n_padded::Integer,
-    nlag::Integer,
-    center::Bool = true
-)
-
-    accum .= 0
-    out_norm = zero(eltype(out))
-    for vecno = 1:nu
-        out_norm += _xcorr!(
-            accum,
-            us[vecno],
-            u_ls[vecno],
-            vs[vecno],
-            v_ls[vecno],
-            r_buf,
-            uim_buf,
-            vim_buf,
-            p,
-            ip,
-            center
-        )
-    end
-
-    circ_xcorr_unwrap!(out, accum, nlag, n_padded)
-    out ./= out_norm * n_padded
-    out
-end
-
-function prepare_circ_xcorr(
-    us::AbstractArray{<:AbstractArray{<:Real}},
-    vs::AbstractArray{<:AbstractArray{<:Real}},
-    outtype::Type = Float32,
-)
-    nu = length(us)
-    nu == length(vs) || throw(ArgumentError("us and vs not the same length"))
-    u_ls = length.(us)
-    u_max_l = maximum(u_ls)
-    v_ls = length.(vs)
-    v_max_l = maximum(v_ls)
-    nlag = max(u_max_l, v_max_l) - 1
-    n = 2 * nlag + 1
-
-    n_padded = nextfastfft(n)
-    n_comp = div(n_padded, 2) + 1
-
-    r_buf = Vector{outtype}(undef, n_padded)
-    accum = similar(r_buf)
-    uim_buf = Vector{Complex{outtype}}(undef, n_comp)
-    vim_buf = similar(uim_buf)
-    out = Vector{outtype}(undef, n)
-
-    p = plan_rfft(r_buf)
-    ip = plan_brfft(uim_buf, n_padded)
-
-    return (
+    ulen = length(u)
+    length(v) == ulen || error("Don't know what to do")
+    ulen > 2 * nlag || error("u is not long enough")
+    (
         out,
         accum,
         r_buf,
         uim_buf,
         vim_buf,
-        nu,
-        u_ls,
-        v_ls,
         p,
         ip,
-        n_padded,
-        nlag
+        bufflen
+    ) = prepare_xcorr(ulen, nlag, outeltype)
+    out_norm = _xcorr!(
+        accum, u, ulen, v, r_buf, bufflen, uim_buf, vim_buf, p, ip, nlag, center
     )
+    xcorr_unwrap!(out, accum, nlag, bufflen)
+    out ./= out_norm
+    out
 end
 
-function circ_xcorr_normed(
+# Version for many vectors
+function xcorr_normed_valid(
     us::AbstractArray{<:AbstractArray{<:Real}},
-    vs::AbstractArray{<:AbstractArray{<:Real}};
+    vs::AbstractArray{<:AbstractArray{<:Real}},
+    nlag::Integer;
     outtype::Type = Float32,
     center::Bool = true
 )
-    out, accum, r_buf, uim_buf, vim_buf, nu, u_ls, v_ls, p, ip, n_padded, nlag =
-        prepare_circ_xcorr(us, vs, outtype)
-    _xcorr_normed!(
+    out, accum, r_buf, uim_buf, vim_buf, p, ip, bufflen, nu, u_ls =
+        prepare_xcorr(us, vs, nlag, outtype)
+    _xcorr_normed_valid!(
         out,
         accum,
         r_buf,
@@ -128,12 +47,115 @@ function circ_xcorr_normed(
         vs,
         nu,
         u_ls,
-        v_ls,
         p,
         ip,
-        n_padded,
+        bufflen,
         nlag,
         center
+    )
+end
+
+function _xcorr_normed_valid!(
+    out::AbstractVector{<:Real},
+    accum::AbstractVector{<:Real},
+    r_buf::AbstractVector{<:Real},
+    uim_buf::AbstractVector{<:Complex},
+    vim_buf::AbstractVector{<:Complex},
+    us::AbstractArray{<:AbstractArray{<:Real}},
+    vs::AbstractArray{<:AbstractArray{<:Real}},
+    nu::Integer,
+    u_ls::AbstractVector{<:Integer},
+    p,
+    ip,
+    bufflen::Integer,
+    nlag::Integer,
+    center::Bool = true
+)
+    accum .= 0
+    out_norm = zero(eltype(out))
+    for vecno = 1:nu
+        out_norm += _xcorr!(
+            accum,
+            us[vecno],
+            u_ls[vecno],
+            vs[vecno],
+            r_buf,
+            bufflen,
+            uim_buf,
+            vim_buf,
+            p,
+            ip,
+            nlag,
+            center
+        )
+    end
+
+    xcorr_unwrap!(out, accum, nlag, bufflen)
+    out ./= out_norm
+    out
+end
+
+function prepare_xcorr(nu, nlag, outeltype)
+    bufflen = nextfastfft(2 * nu - 1)
+    complen = div(bufflen, 2) + 1
+    outlen = 2 * nlag + 1
+
+    r_buf = Vector{outeltype}(undef, bufflen)
+    out = similar(r_buf, outlen)
+    accum = zeros(outeltype, bufflen)
+
+    uim_buf = Vector{Complex{outeltype}}(undef, complen)
+    vim_buf = similar(uim_buf)
+
+    p = plan_rfft(r_buf)
+    ip = plan_irfft(uim_buf, bufflen)
+
+    return (
+        out,
+        accum,
+        r_buf,
+        uim_buf,
+        vim_buf,
+        p,
+        ip,
+        bufflen
+    )
+end
+
+function prepare_xcorr(
+    us::AbstractArray{<:AbstractArray{<:Real}},
+    vs::AbstractArray{<:AbstractArray{<:Real}},
+    nlag::Integer,
+    outtype::Type = Float32,
+)
+    nu = length(us)
+    nu == length(vs) || throw(ArgumentError("us and vs not the same length"))
+    u_ls = length.(us)
+    all(u_ls .== length.(vs)) || throw(ArgumentError("elements of us must be same len as vs"))
+    u_max_l = maximum(u_ls)
+
+    (
+        out,
+        accum,
+        r_buf,
+        uim_buf,
+        vim_buf,
+        p,
+        ip,
+        bufflen
+    ) = prepare_xcorr(u_max_l, nlag, outtype)
+
+    return (
+        out,
+        accum,
+        r_buf,
+        uim_buf,
+        vim_buf,
+        p,
+        ip,
+        bufflen,
+        nu,
+        u_ls
     )
 end
 
@@ -141,27 +163,44 @@ end
 # before call, r_buf should have padded data
 # After call, r_buf will have centered data
 # im_buf will be overwritten to have the fourier transform of r_buf after call
-function pad_transform!(u, ulen, r_buf, im_buf, p, center)
-    copyto!(r_buf, 1, u, 1, ulen)
-    r_buf[(ulen + 1):end] .= 0
+function pad_transform!(u, ulen, r_buf, buflen, im_buf, p, nlag, center)
+    adj_len = ulen - 2 * nlag
+    center_ndx = div(ulen - 1, 2) + 1
+    nleft = div(adj_len - 1, 2)
+    nright = adj_len - nleft
+    copyto!(r_buf, 1, u, center_ndx, nright)
+    left_ndx = buflen - nleft + 1
+    copyto!(r_buf, left_ndx, u, 1 + nlag, nleft)
+    r_buf[nright + 1:left_ndx - 1] .= 0
     if center
-        r_buf[1:ulen] .-= mean(u)
+        mu = mean(u)
+        r_buf[1:nright] .-= mu
+        r_buf[left_ndx:buflen] .-= mu
     end
     mul!(im_buf, p, r_buf)
 end
 
+function rbuf_norm(r_buf, nminright, minleftndx, buflen)
+    sum(abs2, view(r_buf, 1:nminright)) +
+        sum(abs2, view(r_buf, minleftndx:buflen))
+end
+
 # Kernel for xcorr
 function _xcorr!(
-    accum, u, ulen, v, vlen, r_buf, uim_buf, vim_buf, p, ip, center
+    accum, u, ulen, v, r_buf, buflen, uim_buf, vim_buf, p, ip, nlag, center
 )
+    adj_len = ulen - 2 * nlag
+    min_side_l = div(adj_len - 1, 2)
+    nminright = adj_len - min_side_l
+    minleftndx = buflen - min_side_l + 1
     # Transform u
-    pad_transform!(u, ulen, r_buf, uim_buf, p, center)
-    unorm = sum(abs2, view(r_buf, 1:ulen))
+    pad_transform!(u, ulen, r_buf, buflen, uim_buf, p, nlag, center)
+    unorm = rbuf_norm(r_buf, nminright, minleftndx, buflen)
     # Transform v
-    pad_transform!(v, vlen, r_buf, vim_buf, p, center)
-    vnorm = sum(abs2, view(r_buf, 1:vlen))
+    pad_transform!(v, ulen, r_buf, buflen, vim_buf, p, 0, center)
+    vnorm = rbuf_norm(r_buf, nminright, minleftndx, buflen)
     # Multiply to do cross correlation in fourier domain
-    uim_buf .= uim_buf .* conj.(vim_buf)
+    uim_buf .*= conj.(vim_buf)
     # inverse fourier transform
     mul!(r_buf, ip, uim_buf)
     # Add to accumulator
@@ -170,7 +209,7 @@ function _xcorr!(
     sqrt(unorm * vnorm)
 end
 
-function circ_xcorr_unwrap!(out, accum, nlag, n_padded)
+function xcorr_unwrap!(out, accum, nlag, n_padded)
     copyto!(out, nlag + 1, accum, 1, nlag + 1)
     copyto!(out, 1, accum, ndx_offset(n_padded, -nlag), nlag)
     out
@@ -186,97 +225,71 @@ function xcorr_basis(nxcorr)
     -maxlag:maxlag
 end
 
-function circ_xcorr_sig(
-    ib::Integer,
-    ie::Integer,
+function xcorr_valid_sig(
     xc::AbstractVector{<:Number},
     xc_nulls::AbstractVector{<:Number},
     ntrial = length(null_xcs)
 )
-    xc_extr_idx = argmax(abs.(view(xc, ib:ie)))
-    xc_extr = xc[ndx_offset(ib, xc_extr_idx)]
+    xcmin, xcmax = extrema(xc)
+    xc_extr = ifelse(abs(xcmin) > abs(xcmax), xcmin, xcmax)
     mc_twotail_asymm_p(xc_extr, xc_nulls, ntrial)
 end
 
-function circ_xcorr_sig(
-    chunks_u::AbstractVector{<:SharedVector},
-    chunks_v::AbstractVector{<:SharedVector},
+function xcorr_valid_sig(
+    nlag::Integer,
+    chunks_u::AbstractVector{<:AbstractVector},
+    chunks_v::AbstractVector{<:AbstractVector},
     xcs_null::AbstractVector{<:Number},
-    ib::Integer,
-    ie::Integer,
     n_trial = length(xcs_null),
     center::Bool = false
 )
-    xc = circ_xcorr_normed(chunks_u, chunks_v, center = center)
-    circ_xcorr_sig(ib, ie, xc, xcs_null, n_trial)
+    xc = xcorr_normed_valid(chunks_u, chunks_v, nlag, center = center)
+    xcorr_valid_sig(xc, xcs_null, n_trial)
 end
 
-function circ_xcorr_sig(
-    chunks_u::AbstractVector{<:SharedVector},
-    chunks_v::AbstractVector{<:SharedVector},
-    ib::Integer,
-    ie::Integer,
+function xcorr_valid_sig(
+    nlag::Integer,
+    chunks_u::AbstractVector{<:AbstractVector},
+    chunks_v::AbstractVector{<:AbstractVector},
     n_trial = 1000,
     center::Bool = false
 )
-    xcs_null = circ_xcorr_null_mc(chunks_u, chunks_v, ib, ie, n_trial, center)
-    circ_xcorr_sig(chunks_u, chunks_v, xcs_null, ib, ie, n_trial, center)
+    xcs_null = xcorr_null_mc(chunks_u, chunks_v, nlag, n_trial, center)
+    xcorr_valid_sig(nlag, chunks_u, chunks_v, xcs_null, n_trial, center)
 end
 
-function circ_xcorr_null_mc(
-    chunks_u::AbstractVector{<:SharedVector{E}},
-    chunks_v::AbstractVector{<:SharedVector{F}},
-    ib,
-    ie,
-    n_trial = 1000,
+function xcorr_null_mc(
+    us::AbstractVector{<:AbstractVector},
+    vs::AbstractVector{<:AbstractVector},
+    nlag::Integer,
+    n_trial::Integer = 1000;
+    outtype::Type = Float32,
     center::Bool = false
-) where {E, F}
-    outtype = promote_type(E, F)
-    nu = length(chunks_u)
-    length(chunks_v) == nu || throw(ArgumentError("Chunks not the same length"))
+)
     extr_out = Vector{outtype}(undef, n_trial)
-    (
-        xc_out,
-        accum,
-        r_buf,
-        uim_buf,
-        vim_buf,
-        nu,
-        u_ls,
-        v_ls,
-        pl,
-        ipl,
-        n_padded,
-        nlag
-    ) = prepare_circ_xcorr(chunks_u, chunks_v)
+    out, accum, r_buf, uim_buf, vim_buf, p, ip, bufflen, nu, us_ls =
+        prepare_xcorr(us, vs, nlag, outtype)
 
     for i = 1:n_trial
-        p = randperm_notsame(nu)
-        shuff_vs = chunks_v[p]
-        shuff_v_ls = v_ls[p]
-        _xcorr_normed!(
-            xc_out,
+        rp = randperm_notsame(nu)
+        shuff_vs = vs[rp]
+        _xcorr_normed_valid!(
+            out,
             accum,
             r_buf,
             uim_buf,
             vim_buf,
-            chunks_u,
+            us,
             shuff_vs,
             nu,
-            u_ls,
-            shuff_v_ls,
-            pl,
-            ipl,
-            n_padded,
+            us_ls,
+            p,
+            ip,
+            bufflen,
             nlag,
             center
         )
-        xc_max = typemin(eltype(xc_out))
-        xc_min = typemax(eltype(xc_out))
-        for j = ib:ie
-            xc_max = max(xc_max, xc_out[j])
-            xc_min = min(xc_min, xc_out[j])
-        end
+        xc_min, xc_max = extrema(out)
         extr_out[i] = ifelse(abs(xc_max) > abs(xc_min), xc_max, xc_min)
     end
     extr_out
